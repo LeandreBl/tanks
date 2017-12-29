b4c7e3d45e803528d385c1cc81bdf43a8470b589
Leandre Blanchard <leandre.blanchard@netatmo.com>
2017-12-29 17:41:24 +0100

    update lib : TODO change the code to match the new lib version update

--- include/my.h
+++ include/my.h
@@ -5,7 +5,7 @@
 ** Login   <lblanchard@epitech.net>
 ** 
 ** Started on  Thu Oct 13 09:19:01 2016 Leandre Blanchard
-** Last update Sat Nov  4 03:19:41 2017 Léandre Blanchard
+** Last update Mon Dec 25 02:04:47 2017 Léandre Blanchard
 */
 
 #ifndef MY_H_
@@ -19,6 +19,27 @@ extern "C" {
 }
 #endif
 # include <stddef.h>
+# include <stdbool.h>
+# include <stdint.h>
+
+/*
+** Return a server socket on port <port>
+** Returns -1 on error
+*/
+int	create_server(uint16_t port);
+
+/*
+** Return a socket when a new client is connected, <sockfd> is the server fd
+** <backlog> argument defines the length of the kernel pending queue
+** Returns -1 on error
+*/
+int	accept_client(int sockfd, int backlog);
+
+/*
+** Try to connect to the server <ip> on <port> and return a client socket
+** Return -1 on error
+*/
+int	connect_to(const char *ip, uint16_t port);
 
 /* swap the inside of both values swap(&i, &j) */
 void	swap(void *a, void *b);
@@ -28,7 +49,7 @@ void	my_memset(void *ptr, int value, size_t size);
 
 /*
 ** Isacmd returns 1 if the cmd exist in $PATH
-** return 0 if not or on error
+** return 0 otherwise
 */
 int	isacmd(const char *str);
 
@@ -143,7 +164,7 @@ int	sort_tab(char **tabptr);
 ** into an allocated NULL terminated tab
 ** Returns NULL on error
 */
-char	**dir_filenames(const char *dirname, int sort);
+char	**dir_filenames(const char *dirname, bool sort);
 
 /*
 ** Allocate strlen(str) + size bytes,
@@ -184,11 +205,13 @@ int	add_to_clipboard(const char *str);
 */
 int	nb_of(const char *s, char cmp);
 
-/* We'll not talk about this */
+/* my malloc and my free, using only sbrk */
 void	*my_malloc(size_t size);
-
-/* Like really not at all */
-void	free_my(void);
+int	my_free(void *ptr);
+/* free all my_malloc allocated pointers */
+int	my_frees(void);
+/* my_stack gives you the opportunity to see what's inside your my_malloc stack */
+void	my_stack(void);
 
 /* 
 ** Displays the inside of the pointed [buf] memory first [size] bytes
@@ -199,7 +222,7 @@ void	free_my(void);
 ** The display format is [<byte1>][<byte2>][<byte3>] ...
 ** Returns -1 on error
 */
-int	display_inside(const void *buf, const char *format, int size);
+int	display_inside(const void *buf, const char *format, size_t size);
 
 /* Returns the largest strlen(tab[i]) found into the NULL terminated tab */
 int	max_len(char **tabptr);
@@ -256,7 +279,7 @@ int	tab_remove(void *tabptr, int index);
 ** [end] string like end_with("foo.mp3", ".mp3);
 ** Returns 0 if not :)
 */
-int	end_with(const char *s, const char *end);
+bool	end_with(const char *s, const char *end);
 
 /*
 ** Kinda same as my_realloc but with a tab, so you can add
@@ -367,7 +390,7 @@ int	total_name(const char *);
 /*
 ** Man 3 bzero
 */
-int	zeros(char *, int);
+int	zeros(void *ptr, size_t size);
 
 void	my_putchar(char);
 
